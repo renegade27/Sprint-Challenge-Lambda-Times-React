48514829ab36e48c5a5df142c8d5d5c4c6f39812
Zero <jrr043@latech.edu>
2019-03-23 14:26:06 -0500

    MVP

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBarComp = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+  color: white;
+  padding: 0 20px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const LeftContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+const CenterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  padding: 0 20px;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <TopBarComp>
+      <Container>
+        <LeftContainer>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </LeftContainer>
+        <CenterContainer>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </CenterContainer>
+        <RightContainer>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+        </RightContainer>
+      </Container>
+    </TopBarComp>
   )
 }
 
